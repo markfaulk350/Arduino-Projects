8ab8fdb197d04c30eace05cc0cc70514f77575bf
Mark Faulkner <markfaulk350@gmail.com>
2020-02-18 08:48:58 -0800

    fast led isnt working on my imac so i am switching files to laptop

--- controll_neopixel/controll_neopixel.ino
+++ controll_neopixel/controll_neopixel.ino
@@ -1,9 +1,9 @@
 #include <FastLED.h>
 
-#define LED_PIN 7   // Arduino Uno digital pin connecting to WS2812 data-in
-#define NUM_LEDS 3  // # of WS2812 leds on strip
+#define LED_PIN 7  // Arduino Uno digital pin connecting to WS2812 data-in
+#define NUM_LEDS 3 // # of WS2812 leds on strip
 
-int commits[] = {0, 1, 5};
+// int commits[] = {0, 1, 5};
 
 // 1st (235, 234, 240)
 // 2nd (155, 226, 167)
@@ -13,33 +13,51 @@ int commits[] = {0, 1, 5};
 
 CRGB led[NUM_LEDS];
 
-void setup() {
+void setup()
+{
   // FastLED.addLeds<WS2812, LED_PIN, GRB>(led, NUM_LEDS);
   FastLED.addLeds<NEOPIXEL, LED_PIN>(led, NUM_LEDS);
+  led[0] = CRGB(255, 0, 0);
+    FastLED.show();
 }
 
-void loop() {
-  led[0] = CRGB(235, 234, 240);
-  FastLED.show();
-  delay(500);
+void loop()
+{
+  for (int i = 0; i < 3; i++)
+  {
+    led[i] = CRGB(255, 0, 0);
+    FastLED.show();
+    delay(100);
+  }
 
-  led[1] = CRGB(155, 226, 167);
-  FastLED.show();
-  delay(500);
+  for (int i = 0; i < 3; i++)
+  {
+    led[i] = CRGB(0, 0, 0);
+    FastLED.show();
+    delay(100);
+  }
 
-  led[2] = CRGB(84, 205, 101);
-  FastLED.show();
-  delay(500);
+  // led[0] = CRGB(235, 234, 240);
+  // FastLED.show();
+  // delay(500);
 
-  led[0] = CRGB(56, 156, 68);
-  FastLED.show();
-  delay(500);
+  // led[1] = CRGB(155, 226, 167);
+  // FastLED.show();
+  // delay(500);
 
-  led[1] = CRGB(33, 96, 42);
-  FastLED.show();
-  delay(500);
+  // led[2] = CRGB(84, 205, 101);
+  // FastLED.show();
+  // delay(500);
 
-  led[2] = CRGB(235, 234, 240);
-  FastLED.show();
-  delay(500);
+  // led[0] = CRGB(56, 156, 68);
+  // FastLED.show();
+  // delay(500);
+
+  // led[1] = CRGB(33, 96, 42);
+  // FastLED.show();
+  // delay(500);
+
+  // led[2] = CRGB(235, 234, 240);
+  // FastLED.show();
+  // delay(500);
 }
